f4069de8921f09449edf739a8dec04e870bf1cb9
Tamir <shklazt@gmail.com>
2018-09-13 16:59:53 +0200

    finished project

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import time
 from datetime import datetime
+import os
+
 
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
+import RPi.GPIO as GPIO
 
 # Software SPI configuration:
 CLK = 18
@@ -13,6 +16,67 @@ MOSI = 24
 CS = 25
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
+
+# STATE
+freq = 0.5
+timer = 0
+on = True
+readings = []
+
+
+def reset(channel=False):
+    os.system('clear')
+    global timer
+    timer = 0
+
+
+def change_freq(channel=False):
+    global freq
+    print(freq)
+    if freq == 0.5:
+        freq = 1
+    elif freq == 1:
+        freq = 2
+    elif freq == 2:
+        freq = 0.5
+
+
+def stop(channel=False):
+    global on, readings
+    readings = []
+    on =  not on
+
+def display(channel=False):
+    print("FIRST FIVE READINGS SINCE LAST STOP")
+    global readings
+    for reading in readings:
+        print(reading)
+
+
+# PINS
+GPIO.setmode(GPIO.BCM)
+
+FREQ_PIN = 21
+RESET_PIN = 20
+STOP_PIN=16
+DISPLAY_PIN=12
+
+GPIO.setup(FREQ_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(FREQ_PIN, GPIO.RISING,
+                      callback=change_freq, bouncetime=300)
+
+GPIO.setup(RESET_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(RESET_PIN, GPIO.RISING,
+                      callback=reset, bouncetime=300)
+
+GPIO.setup(STOP_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(STOP_PIN, GPIO.RISING,
+                      callback=stop, bouncetime=300)
+
+GPIO.setup(DISPLAY_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(DISPLAY_PIN, GPIO.RISING,
+                      callback=display, bouncetime=300)
+
 # Hardware SPI configuration:
 # SPI_PORT   = 0
 # SPI_DEVICE = 0
@@ -20,7 +84,8 @@ mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 
 def convert_pot(adc_reading):
-    return round((adc_reading/1024.0)*3.3,1)
+    return round((adc_reading/1024.0)*3.3, 1)
+
 
 def convert_ldr(adc_reading):
     norm = adc_reading-30
@@ -34,17 +99,25 @@ column_headers = ['Time', 'Timer', 'Pot', 'Temp', 'Light']
 print('| {0:>8} | {1:>8} | {2:>6} | {3:>4} | {4:>6} |'.format(*column_headers))
 print('-' * 57)
 # Main program loop.
-timer = 0
-while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*8
-    values[0] = time.strftime('%H:%M:%S')
-    values[1] = datetime.fromtimestamp(timer).strftime('%H:%M:%S')
-    values[2] = convert_pot(mcp.read_adc(7))
-    values[3] = mcp.read_adc(6)
-    values[4] = convert_ldr(mcp.read_adc(5))
+try:
+    while True:
+        # Read all the ADC channel values in a list.
+        
 
-    # Print the ADC values.
-    print('| {0:>8} | {1:>8} | {2:>4} V | {3:>4} | {4:>5}% |'.format(*values))
-    # Pause for half a second.
-    time.sleep(2)
+        values = [0]*8
+        values[0] = time.strftime('%H:%M:%S')
+        values[1] = datetime.fromtimestamp(timer).strftime('%H:%M:%S')
+        values[2] = convert_pot(mcp.read_adc(7))
+        values[3] = mcp.read_adc(6)
+        values[4] = convert_ldr(mcp.read_adc(5))
+
+        if len(readings) <= 5:
+            readings.append('| {0:>8} | {1:>8} | {2:>4} V | {3:>4} | {4:>5}% |'.format(*values))
+
+        # Print the ADC values.
+        if on:
+            print('| {0:>8} | {1:>8} | {2:>4} V | {3:>4} | {4:>5}% |'.format(*values))
+        # Pause for half a second.
+        time.sleep(freq)
+except KeyboardInterrupt:
+    GPIO.cleanup()
