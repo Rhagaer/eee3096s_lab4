ce3b58584fccbb99f32e3fe265010ab0e32a7569
Tamir <shklazt@gmail.com>
2018-09-13 14:32:17 +0200

    Pot conversion working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
@@ -6,10 +7,10 @@ import Adafruit_MCP3008
 
 
 # Software SPI configuration:
-CLK  = 18
+CLK = 18
 MISO = 23
 MOSI = 24
-CS   = 25
+CS = 25
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Hardware SPI configuration:
@@ -18,19 +19,28 @@ mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 # mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 
+def convert_pot(adc_reading):
+    
+    return round((adc_reading/1024.0)*3.3,1)
+
+
 print('Reading MCP3008 values, press Ctrl-C to quit...')
 # Print nice channel column headers.
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+column_headers = ['Time', 'Timer', 'Pot', 'Temp', 'Light']
+print('| {0:>8} | {1:>8} | {2:>4} | {3:>4} | {4:>5} |'.format(*column_headers))
 print('-' * 57)
 # Main program loop.
+timer = 0
 while True:
     # Read all the ADC channel values in a list.
     values = [0]*8
-    for i in range(8):
-        # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
+    values[0] = time.strftime('%H:%M:%S')
+    values[1] = datetime.fromtimestamp(timer).strftime('%H:%M:%S')
+    values[2] = convert_pot(mcp.read_adc(7))
+    values[3] = mcp.read_adc(6)
+    values[4] = mcp.read_adc(7)
+
     # Print the ADC values.
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    print('| {0:>8} | {1:>8} | {2:>4}V | {3:>4} | {4:>5} |'.format(*values))
     # Pause for half a second.
-    time.sleep(1)
-    print("===NEW READING===")
+    time.sleep(2)
